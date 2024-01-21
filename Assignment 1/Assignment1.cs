df5d7d8dcae9b959dd1ddaafe5cda95ce75115b6
mattjhellard <matthewhellard@trentu.ca>
2024-01-21 11:24:29 -0500

    Completed up to and including DoubleCapacity

--- Assignment 1/Assignment1.cs
+++ Assignment 1/Assignment1.cs
@@ -17,20 +17,50 @@ public class ServerGraph // Working on this today, see branch -MH
     // Create an empty server graph
     public ServerGraph()
     {
-
+        //we want to start with the smallest sizes reasonably possible for an "empty" graph, will automatically grow as needed without user intervention
+        V = new WebServer[1];
+        E = new bool[1, 1];
+        NumServers = 0; // observe that numServers is currently 0 while the length of V is 1, this discrepancy is why the NumServers data member is necessary
     }
+
     // 2 marks
     // Return the index of the server with the given name; otherwise return -1
     private int FindServer(string name)
     {
-        // PLACEHOLDER:
-        return -1;
+        for (int i = 0; i < NumServers; i++) //using NumServers here as it will often be smaller than V.Length and there will never be a server at an i>NumServers 
+        {
+            if (V[i].Name == name)
+            {
+                return i; //if the current server name matches the supplied name return the current index  
+            }
+        }
+        return -1; //if the process gets here then there are no servers in the array with the supplied name, so return -1
     }
+
     // 3 marks
     // Double the capacity of the server graph with the respect to web servers
     private void DoubleCapacity()
     {
+        // creating newV to populate with V values before replacing V with newV
+        WebServer[] newV = new WebServer[V.Length * 2];
+        // populating newV with V values
+        for (int i = 0; i < V.Length; i++) //No need to iterate past V.Length, in fact we shouldn't as the index would go out of bounds
+        {
+            newV[i] = V[i]; //Note: because WebServer is a reference type you may consider messing with cloning to achieve this, however if you draw out what happens without cloning it actually makes more sense than with cloning for the context of servers
+        }
+        // we're going to hold off on reassigning V until E is also successfully prepared for reassignment, though V is now prepared at this point
 
+        bool[,] newE = new bool[newV.Length, newV.Length]; // E will always be a square matrix with width/height equal to V, this is also why we worked with V first btw
+        for (int ix = 0; ix < V.Length; ix++)
+        { //again, no need to iterate past V.length and it is also a bad idea to do so
+            for (int iy = 0; iy < V.Length; iy++) //classic nested for to traverse multidimensional array
+            {
+                newE[ix, iy] = E[ix, iy]; //because bools are NOT reference types there is no need (or option?) to consider cloning here 
+            }
+        }
+        //At this point both V and E are ready to be reassigned
+        V = newV;
+        E = newE;
     }
     // 3 marks
     // Add a server with the given name and connect it to the other server
@@ -182,7 +212,8 @@ public class Tester
 {
     public static void Main()
     {
-        Console.WriteLine("Test Test 123 123 :)");
+        ServerGraph testies = new ServerGraph();
+        Console.WriteLine(testies); //we don't expect a pretty or useful output for this
         Console.ReadLine(); // in case you've forgotten this is here to circumvent the console window from closing on completion (by technically preventing completion)
     }
 }

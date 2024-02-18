3d1cb84445b277f5d3f705a4093ba03558ed1dcb
mattjhellard <matthewhellard@trentu.ca>
2024-02-18 15:03:32 -0500

    Added template- NOTE: View -> Task List for panel showing all TODOs formally

--- Assignment 2/Assignment2.cs
+++ Assignment 2/Assignment2.cs
@@ -1,2 +1,80 @@
 ﻿using System;
-// TODO: Do assignment
+using Node = Rope; // iirc "nodes" in this context are same as "ropes" and vice versa, this makes it official, can refactor away if need be
+
+public class Rope
+{
+    public Rope(string S)
+    {
+        // TODO: implement constructor (unclaimed)
+    }
+    private Node Build(string s, int i, int j)
+    {
+        // TODO: implement method Build (unclaimed)
+        return null; //placeholder
+    }
+    private Node Concatenate(Node p, Node q)
+    {
+        // TODO: implement method Concatenate (unclaimed)
+        return null; //placeholder
+    }
+    private Node Split(Node p, int i)
+    {
+        // TODO: implement method Split (unclaimed)
+        return null; //placeholder
+    }
+    private Node Rebalance()
+    {
+        // TODO: implement method Rebalance (unclaimed)
+        return null; //placeholder
+    }
+    public void Insert(string S, int i)
+    {
+        // TODO: implement method Insert (unclaimed)
+    }
+    public void Delete(int i, int j)
+    {
+        // TODO: implement method Delete (unclaimed)
+    }
+    public string Substring(int i, int j)
+    {
+        // TODO: implement method Substring (unclaimed)
+        return null; //placeholder
+    }
+    public int Find(string S)
+    {
+        // TODO: implement method Find (unclaimed)
+        return -1; //placeholder
+    }
+    public char CharAt(int i)
+    {
+        // TODO: implement method CharAt (unclaimed)
+        return '\0'; //placeholder
+    }
+    public int IndexOf(char c)
+    {
+        // TODO: implement method IndexOf (unclaimed)
+        return '\0'; //placeholder
+    }
+    public void Reverse()
+    {
+        // TODO: implement method Reverse (unclaimed)
+    }
+    public int Length()
+    {
+        // TODO: implement method Length (unclaimed)
+        return -1; //placeholder
+    }
+    public string ToString()
+    {
+        // TODO: implement method ToString (unclaimed)
+        return null; //placeholder
+    }
+}
+
+public static class User
+{
+    public static void Main(string[] args) // NOTE: let's consider implementing accepting initial arguments, if we don't do that then remove parameters from main before submission
+    {
+        // TODO: implement main (MH)
+    }
+}

f59c3d05c045a0a5a2ff8e6164d1fcd8b5c4e976
mattjhellard <matthewhellard@trentu.ca>
2024-02-21 11:15:56 -0500

    Modified Main framework, PrintRope, various little things

--- Assignment 2/Assignment2.cs
+++ Assignment 2/Assignment2.cs
@@ -8,7 +8,6 @@ public class Rope
     private Node leftChild, rightChild;
     private String value; // TODO: find better name for this data member
 
-
     // (5 marks) Create a balanced rope from a given string S
     public Rope(string S) // due to the recursive nature of the rope, this being the only constructor, and the fact that Build has to create a new Node somehow, we arrive at the conclusion that the constructor is going to be called within that recursion, and as such handles much of the recursion logic itself
     {
@@ -20,12 +19,13 @@ public class Rope
         }
         else //if the given string still needs to be broken down some more
         {
-            value = ""; //we use the empty string to represent parent nodes' lack of a value for this datamember, makes printing them easier
-            int splitIndex = (int)totalLength / 2; //iirc this cast to int will effectively just drop the decimal value of the resultant float, i.e, it will always round down to form an int, this is okay as long as we keep that in mind going forward
-            leftChild = Build(S, 0, splitIndex); //build basically returns whatever THIS constructor makes when the input is the designated substring
+            value = ""; //we use the empty string to represent parent nodes' lack of a value for this datamember, though as of writing this value is never used in a parent
+            int splitIndex = (int)totalLength / 2; //this cast effectively rounds down, the result of this is if totalLength is odd then the right child always gets the extra char
+            leftChild = Build(S, 0, splitIndex); //build basically returns whatever THIS constructor makes when the substring defined in the arguments is fed back into it
             rightChild = Build(S, splitIndex, totalLength); // TODO: explain why the end of leftChild should have the same value as the start of rightChild
         }
     }
+
     // Recursively build a balanced rope for S[i,j] and return its root
     private Node Build(string s, int i, int j)
     { //as this is a private method we can assume methods calling it will simply use it properly, and then go make sure that's definitely the case
@@ -92,12 +92,11 @@ public class Rope
     }
     // (1 mark) Return the length of the string
     public int Length()
-    {
-        // TODO: implement method Length (unclaimed)
-        return -1; //placeholder
+    { //note: while there's nothing explicitely preventing a child node from having this called, a user has no way of accessing children of the ultimate root of the rope they created, so they can only call this from the ultimate root 
+        return totalLength;
     }
     // (4 marks) Return the string represented by the current scope
-    public override string ToString()
+    public override string ToString() //note: all objects have a (basic) ToString method, usually inherited, so this method is technically an override 
     {
         // TODO: implement method ToString (unclaimed)
         return null; //placeholder
@@ -105,31 +104,29 @@ public class Rope
     // (4 marks) Print the augmented binary tree of the current rope
     public void PrintRope()
     {
-        PrintRopeRecursively(0, this);
-    }
-    private void PrintRopeRecursively(int indent, Node root)
-    {
-        if (root == null) //if we allow ourselves to step into null children and then just check at the start of each recursion, the code is a lot cleaner (one check vs two similar checks)
+        PrintRope(0, this); //we simply use the public method to initiate the private recursive function
+        //using local function vs private method as the function exists only to be used by the parent method, so making usage by other methods possible is semantically incorrect
+        void PrintRope(int indentation, Node root)
         {
-            return;
-        }
-        if (root.leftChild == null && root.rightChild == null) //case when at leaf node
-        {
-            for (int i = 0; i < indent; i++)
+            String indent = new String('\t', indentation);
+            //^ this uses an obscure overload of the String constructor to make our sequence of indendations for us
+            //by building the indent string discretely uptop, we simplify the logic of printing what we want with it's required indentations
+            //as strings are reference types, it would likely be quite the headache to have an actual string passed in the arguments, so building a new one for each instance of the method is basically the only option 
+
+            if (root == null) //if we allow ourselves to step into null children and then just check at the start of each recursion, the code is a lot simpler (one check vs two similar checks), although admittedly the overhead of up to two unnecessary recursions (per node) is probably a lot more expensive than one extra conditional, so we're sacrificing performance for looks here
             {
-                Console.Write("\t"); //looping to write appropriate number of indents for current recursion depth
+                return;
             }
-            Console.WriteLine(root.value);
-        }
-        else //case when at parent node
-        { //recall inorder search
-            PrintRopeRecursively(indent + 1, root.leftChild);
-            for (int i = 0; i < indent; i++)
+            if (root.leftChild == null && root.rightChild == null) //case when at leaf node
             {
-                Console.Write("\t");
+                Console.WriteLine(indent+ "* " + root.value);
             }
-            Console.WriteLine("*"); //we use * to indicate parent node in print, indentation should guarantee that visually segregating parents from leafs is never too challenging regardless
-            PrintRopeRecursively(indent + 1, root.rightChild);
+            else //case when at parent node
+            { //recall inorder search
+                PrintRope(indentation + 1, root.rightChild); //going right to left so that the printed tree more accurately represents a 90 degree counter-clockwise rotation of the true tree
+                Console.WriteLine(indent + "*"); //we use * to indicate a node in print, indentation should guarantee that visually segregating parents from leafs is never too challenging regardless
+                PrintRope(indentation + 1, root.leftChild);
+            }
         }
     }
 }
@@ -146,24 +143,41 @@ public static class User
         {
             Console.Write("Input :");
             //we tokenize the user input (space separated) to enable inputting all required data for a command at once, similar to any real CLI
-            //like a CLI, we want to include options (ex: -l), but to make options work nicely we will restrict data inputs that start with '-' in some contexts to protect from likely data entry mistakes
+            //like a CLI, we want to include options (ex: -l)
             // TODO: IF it proves more work than it's worth to implement decently, remove options functionality, disregard this otherwise
-            String[] input = Console.ReadLine().Split(' ');
+            String[] input = Console.ReadLine().Trim(' ').Split(' ');
+
+            //this section defines the default values for all potential arguments, if the value changes from the default, then it was successfully given as an argument
+            //the names may look a little cryptic but they're just the relevant command and relevant flag stuck together
+            String NRp = null;
+
+            //this loop runs through the user input and assigns arguments for flags as appropriate, it does not make particular effort to protect the user from data entry errors
+            for (int i = 1; i < input.Length - 1; i++) //we need not check the first arg (always a command) or the last arg (never a flag), not entering the loop with the last arg also helps prevent index out of bounds errors
+            {
+                switch (input[i])
+                {
+                    case "-p":
+                        NRp = input[i + 1];
+                        i++; //we just assigned i+1 as an argument for a flag, so we need not run this loop for i+1 at all
+                        break;
+                }
+            }
             switch (input[0].ToLower()) //we don't want to change case of whole input as data inputted should allow for case-sensitivity, but command (first substring) can ignore this and modify case for convenience
             {
                 case "nr":
                 case "new rope":
-                    if (input.Length == 2 && input[1][0] != '-') //idk if this is common knowledge but apparently c# still treats strings like arrays of chars to the point where you can reference chars at an index in a string with []
-                    { //in the case of a user inputting something like "nr -p" with no actual argument given post-option, we want to prevent the option becoming the input
-                        rope = new Rope(input[1]);
-                    }
-                    else if (input.Length == 3 && input[1].Equals("-p")) //-p means path, we will try to find a matching file and use it's contents as the new string input
-                    {//TODO: IF file input not implemented remove this whole block before submission
-                        // TODO: (optional) implement file input functionality (unclaimed)
-                    }
-                    else //consider this the default case, if options/arguments past command ultimately fail or aren't given we default to more typical input failure
+                    if (input.Length == 2) //if one arg given after command we assume we are to use it as the input for the rope 
                     {
-                        Console.Write("!: Input new rope string :");
+                        rope = new Rope(input[input.Length - 1]);
+                    }
+                    else if (NRp != null) //if path was given
+                    {
+                        // TODO: add file i/o support, note: we don't want to use file i/o within the rope class, so all that stuff must not leave the User class, save importing the libraries at the start of the file
+                        Console.WriteLine(NRp); //placeholder
+                    }
+                    else //default case
+                    {
+                        Console.Write("Input new rope string :");
                         rope = new Rope(Console.ReadLine());
                     }
                     break;
@@ -181,7 +195,7 @@ public static class User
                     Console.WriteLine(
                         "!: All subsequent arguments/options past command optional, order of options shown is only valid order of input\n" +
                         "-Help (h)\n" + //implemented
-                        "-New Rope ((nr) -> <string to use> || -p <path to file containing string>)\n" + //semi-implemented
+                        "-New Rope (nr) <string to use> || (nr) -p <path to file containing string>\n" + //semi-implemented
                         "-Insert Substring at Index (isi)\n" +
                         "-Delete Substring in Range (dsr)\n" +
                         "-Get Substring in Range (gsr)\n" +
@@ -191,8 +205,8 @@ public static class User
                         "-Reverse Rope (rr)\n" +
                         "-Get Length (gl)\n" +
                         "-Translate To String (tts)\n" +
-                        "-Print Rope (pr)\n" +
-                        "-Quit (q)\n"); //implemented
+                        "-Print Rope (pr)\n" + //implemented
+                        "-Quit (q)"); //implemented
                     break;
                 default:
                     Console.WriteLine("!: unknown input, use (help) for list of valid commands");

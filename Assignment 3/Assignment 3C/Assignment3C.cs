61575b951124b0df32126f6dbfc88951f2e0df43
mattjhellard <matthewhellard@trentu.ca>
2024-04-03 23:47:12 -0400

    Implemented untested unoptomised Insert, untested Print

--- Assignment 3/Assignment 3C/Assignment3C.cs
+++ Assignment 3/Assignment 3C/Assignment3C.cs
@@ -35,8 +35,119 @@ public class TwoThreeFourTree<T> where T : IComparable<T>
     // (6 marks) Insert key k, true if successful, false otherwise
     public bool Insert(T k)
     {
-        // TODO: implement Insert
-        return false; //placeholder
+        if (root.numKeys == 2 * t - 1)
+        {
+            //we have to separately define split-adjacent behavior between when root is full and another node is full as the required behavior is different
+            Node<T> newRoot = new Node<T>(t);
+            newRoot.keys[0] = root.keys[t - 1];
+            newRoot.children[1] = Split(root);
+            newRoot.children[0] = root;
+            root = newRoot;
+        }
+        return InsertRecurse(root);
+
+        bool InsertRecurse(Node<T> p)
+        {
+            //keep in mind we've already handled the case where root is full and since we ultimately start at root we can assume the starting node is never full (in recursions we'll handle full nodes before recursing into them)
+            if (p.leaf)
+            {
+                //if we're at a leaf then the key should go somewhere in here, keep in mind it can't be full at this point
+                for (int i = 0; i < p.numKeys; i++)
+                {
+                    int diff = k.CompareTo(p.keys[i]);
+                    if (diff < 0)
+                    {
+                        for (int j = p.numKeys - 1; j > i; j--)
+                        { //we're shifting the array at and above i right to make room for the new key
+                            p.keys[j] = p.keys[j - 1];
+                        }
+                        //everything has been shifted right so i is now "unoccupied"
+                        p.keys[i] = k;
+                        p.numKeys++;
+                        return true;
+                    }
+                    if (diff == 0)
+                    { //if we find the key already in the tree then we should fail out
+                        return false;
+                    }
+                }
+                //if we get to the end of the (not full) leaf without finding a place for the key, it means the key goes at the end
+                p.keys[p.numKeys] = k; //if there are already two keys then k goes at [2], recall zero indexing
+                p.numKeys++;
+                return true;
+            }
+            else //if we're not at a leaf we should traverse to one
+            {
+                for (int i = 0; i < p.numKeys; i++)
+                {
+                    int diff = k.CompareTo(p.keys[i]);
+                    if (diff < 0)
+                    {
+                        if (p.children[i].numKeys == 2 * t - 1)
+                        { //if node to descend to is full,
+                            for (int j = p.numKeys - 1; j > i; j--)
+                            { //make room for pulled up key,
+                                p.keys[j] = p.keys[j - 1];
+                                p.children[j] = p.children[j - 1];
+                            }
+                            p.keys[i] = p.children[i].keys[t - 1]; //place pulled up key
+                            p.numKeys++;
+                            p.children[i + 1] = Split(p.children[i]); //make the split (pulled up key is dropped from lower table in the operation),
+                            if (k.CompareTo(p.keys[i]) > 0)
+                            { //if the pulled up key should be to the left of our key we increment i to traverse right rather than left of it
+                                i++; //as we're about to return (after a recursion or many) we can abuse our index value like this wihtout concern
+                            }
+                        }
+                        return InsertRecurse(p.children[i]);
+                    }
+                    if (diff == 0)
+                    {
+                        return false;
+                    }
+                }
+                //if we should recurse into the final branch of the node
+                if (p.children[p.numKeys].numKeys == 2 * t - 1)
+                { //if final branch is full, note the logic here is slightly different from if previous branches are full
+                    for (int j = 0; j < p.numKeys; j++)
+                    { //make room for pulled up key, we shift left as we're pulling up to the end
+                        p.keys[j] = p.keys[j + 1];
+                        p.children[j] = p.children[j + 1];
+                    }
+                    p.keys[p.numKeys] = p.children[p.numKeys].keys[t - 1];
+                    p.numKeys++;
+                    Node<T> splitRight = Split(p.children[p.numKeys]);
+                    p.children[p.numKeys - 1] = p.children[p.numKeys];
+                    p.children[p.numKeys] = splitRight;
+                    if (k.CompareTo(p.keys[p.numKeys - 1]) < 0)
+                    {
+                        return InsertRecurse(p.children[p.numKeys - 1]);
+                    }
+                }
+                return InsertRecurse(p.children[p.numKeys]);
+            }
+        }
+        //returns right node resulting from split operation (passed node BECOMES left node) (we drop the key to pull up and assume the caller handles that)
+        Node<T> Split(Node<T> full)
+        {
+            full.leaf = false;
+            Node<T> copy = full;
+            full = new Node<T>(t);
+            Node<T> right = new Node<T>(t);
+            for (int i = 0, j = 0, l = 0; i < 2 * t - 1; i++)
+            {
+                if (i < t - 1)
+                {
+                    full.keys[j] = copy.keys[i];
+                    j++;
+                }
+                else if (i > t - 1)
+                {
+                    right.keys[l] = copy.keys[i];
+                    l++;
+                }
+            }
+            return right;
+        }
     }
 
     // (10 marks) Delete key k, true if successful, false otherwise
@@ -54,7 +165,7 @@ public class TwoThreeFourTree<T> where T : IComparable<T>
         //using a local function so we can run recursively in a semantically sound way (a seperate private method that is only called by this method would be the alternative) and so we can call the recursion without re-passing k
         bool SearchRecurse(Node<T> p)
         {
-            if(p == null) //if we've recursed into a null Node it means the value can't be found
+            if (p == null) //if we've recursed into a null Node it means the value can't be found
             {
                 return false;
             }
@@ -85,7 +196,23 @@ public class TwoThreeFourTree<T> where T : IComparable<T>
     // (4 marks) Print the keys of the 2-3-4 tree in order
     public void Print()
     {
-        // TODO: implement Print
+        PrintRecurse(root, 0);
+        void PrintRecurse(Node<T> p, int indentation)
+        { //we go right to left to accurately represent 90-degree counter-clockwise rotation of tree
+            String indent = new string('\t', indentation); //obscure overload of string that builds sequence of given char to given length
+            if (!p.leaf)
+            {
+                PrintRecurse(p.children[p.numKeys], indentation + 1);
+            }
+            for (int i = p.numKeys-1; i <= 0; i--)
+            {
+                Console.WriteLine(indent + p.keys[i].ToString());
+                if (!p.leaf)
+                {
+                    PrintRecurse(p.children[i], indentation + 1);
+                }
+            }
+        }
     }
 }
 
